92f1a60feaa4ec26bded044f4444f106ea9abf63
brayanricardo13 <brayanricardo13@hotmail.com>
2021-02-17 11:52:16 -0500

    Update Derivados financieros.R

--- Derivados financieros.R
+++ Derivados financieros.R
@@ -1,3 +1,7 @@
 # DERIVADOS FINANCIEROS 
 library(quantmod)
-
+simbolos=c("F","TSLA","GM")
+getSymbols(simbolos,src="yahoo",from="2018-02-11",to="2021-02-17")
+par(mfrow=c(2,1)) # para mostrar dos graficos a la vez
+plot(TSLA$TSLA.Open,main="TSLA",xlab="dates",ylab="Price")
+plot(GM$GM.Open,main="GM",xlab="dates",ylab="Price")
