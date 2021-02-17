16a37cd202c35e21d1ef9bdbb9b1e104b955a760
brayanricardo13 <brayanricardo13@hotmail.com>
2021-02-17 12:44:34 -0500

    Update Derivados financieros.R

--- Derivados financieros.R
+++ Derivados financieros.R
@@ -5,3 +5,4 @@ getSymbols(simbolos,src="yahoo",from="2018-02-11",to="2021-02-17")
 par(mfrow=c(2,1)) # para mostrar dos graficos a la vez
 plot(TSLA$TSLA.Open,main="TSLA",xlab="dates",ylab="Price")
 plot(GM$GM.Open,main="GM",xlab="dates",ylab="Price")
+
